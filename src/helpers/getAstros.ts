47878881e6a8801a0add3c75b05a4c8a64445f34
clintonpi <nkwochaclinton@gmail.com>
2022-02-17 02:33:43 +0100

    fix: hard code the Astros API result
    The API is served over HTTP, therefore, requests to the API made over HTTPS are blocked.

--- src/helpers/getAstros.ts
+++ src/helpers/getAstros.ts
@@ -1,7 +1,50 @@
 import { Astros } from "../models";
 
 export async function getAstros(): Promise<Astros> {
-  const res = await fetch('http://api.open-notify.org/astros.json');
-
-  return res.json();
+  return {
+    people: [
+      {
+        craft: "ISS",
+        name: "Mark Vande Hei"
+      },
+      {
+        craft: "ISS",
+        name: "Pyotr Dubrov"
+      },
+      {
+        craft: "ISS",
+        name: "Anton Shkaplerov"
+      },
+      {
+        craft: "Shenzhou 13",
+        name: "Zhai Zhigang"
+      },
+      {
+        craft: "Shenzhou 13",
+        name: "Wang Yaping"
+      },
+      {
+        craft: "Shenzhou 13",
+        name: "Ye Guangfu"
+      },
+      {
+        craft: "ISS",
+        name: "Raja Chari"
+      },
+      {
+        craft: "ISS",
+        name: "Tom Marshburn"
+      },
+      {
+        craft: "ISS",
+        name: "Kayla Barron"
+      },
+      {
+        craft: "ISS",
+        name: "Matthias Maurer"
+      }
+    ],
+    message: "success",
+    number: 10
+  };
 }
